3f3d051bdfbd13cc6ee25d7bc854627ea77b63d6
HanYoungJu <1025hyj@naver.com>
2019-09-17 21:24:16 +0900

    first commit

---
+++ android/app/src/main/java/com/yajatime/MainActivity.java
@@ -0,0 +1,7 @@
+package com.yajatime;
+
+import com.reactnativenavigation.NavigationActivity;
+
+public class MainActivity extends NavigationActivity {
+
+}
---
+++ ios/Pods/Target Support Files/Pods-Yajatime/Pods-Yajatime-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Yajatime : NSObject
+@end
+@implementation PodsDummy_Pods_Yajatime
+@end
